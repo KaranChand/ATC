a7bb0611dcd7ac0c2a2e448e381e5c8fde9e57c0
KaranChand <karanchand@live.nl>
2022-07-22 15:45:41 +0200

    updated metrics

--- HG/evaluation.py
+++ HG/evaluation.py
@@ -1,23 +1,44 @@
-import evaluate
+from evaluate import load
 from datasets import load_dataset, load_metric
 import pandas as pd 
 from pathlib import Path  
 
-# remove rows that have NaN as model_transcription
-df = pd.read_csv("transcribed_base.csv") 
+# remove rows that have NaN as model_transcription for model evaluation
+filename = "transcribed_base"
+df = pd.read_csv(filename+".csv") 
 clean_df = df.dropna(subset=['model_transcription'])
 clean_df.to_csv(Path("to_eval.csv"), index = False, header=True)
 print("removed %x model_transcriptions that contain NaN" % (len(df) - len(clean_df)))
 
 # load as dataset
 ds = load_dataset('csv', data_files='to_eval.csv', split='train')
+predictions = ds['model_transcription']
+references = ds['transcription']
 
-wer_metric = evaluate.load("wer")
-wer = wer_metric.compute(predictions=ds['model_transcription'], references=ds['transcription'])
-# perplexity = evaluate.load("perplexity", module_type="metric")
-# input_texts = ds[:5]['transcription']
-# results = perplexity.compute(model_id='gpt2',
-#                              input_texts=input_texts)
+# word error rate
+wer_metric = load("wer")
+wer = wer_metric.compute(predictions=predictions, references=references)
 
-# print({"perplexity": round(results["mean_perplexity"], 2)})
-print({"wer": wer})
+# perplexity
+perplexity = load("perplexity", module_type="metric")
+input_texts = references
+results = perplexity.compute(model_id='gpt2',
+                             input_texts=input_texts)
+
+# Character error rate
+cer_metric = load("cer")
+cer = cer_metric.compute(predictions=predictions, references=references)
+
+
+metrics = {"filename" : filename+ '.csv',
+           "perplexity" : results['mean_perplexity'],
+           "wer" : wer,
+           "cer" : cer
+        }
+print(metrics)
+
+df = pd.read_csv("metrics.csv")
+df.set_index('filename', inplace= True)
+df.loc[filename + '.csv'] = metrics
+df.reset_index(inplace=True)
+df.to_csv(Path("metrics.csv"), index = False, header=True)
